f693a871d04366a6309c093bb43ccb8eff3ddaf6
n01302646 <jobanjitsingh0706@gmail.com>
2020-07-22 02:03:48 -0400

    formatting

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -13,20 +13,20 @@ import 'css/default.css';
 const App = () => {
 
     const productArr = [
-		{id: 1, name: 'JBL', price: 50, imgpath:"minni.jpg"},
-		{id: 2, name: 'BOSE', price: 60, imgpath:"ipad.jpg"},
-		{id: 3, name: 'APPLE', price: 70, imgpath:"handsoap.jpg"},
-		{id: 4, name: 'SAMSUNG', price: 80, imgpath:"glass-guard.jpg"},
-		{id: 5, name: 'XIOMI', price: 90, imgpath:"headphone.jpg"}
-	]
+        { id: 1, name: 'JBL', price: 50, imgpath: "minni.jpg" },
+        { id: 2, name: 'BOSE', price: 60, imgpath: "ipad.jpg" },
+        { id: 3, name: 'APPLE', price: 70, imgpath: "handsoap.jpg" },
+        { id: 4, name: 'SAMSUNG', price: 80, imgpath: "glass-guard.jpg" },
+        { id: 5, name: 'XIOMI', price: 90, imgpath: "headphone.jpg" }
+    ]
 
-	return(
+    return (
         <>
-			<Header />
+            <Header />
             <Main />
             <ProductList data={productArr} />
-			<Footer />
-		</>
+            <Footer />
+        </>
     )
 }
 
--- src/js/components/ProductList.js
+++ src/js/components/ProductList.js
@@ -3,18 +3,18 @@ import Product from "components/Product";
 
 
 
-const ProductList = ({data}) => {
+const ProductList = ({ data }) => {
     // Mapped elements MUST have a unique "key" attribute
     const productsArr = data.map(prod => <Product key={prod.id} name={prod.name} price={prod.price} imgpath={prod.imgpath} />)
-    
-	return (
+
+    return (
         <main>
-			<section class="cat_section" id="category">
-				<h2 class="">Categories</h2>
-				<div class="category-grid-box row">
-					{productsArr}
-				</div>
-			</section>
+            <section class="cat_section" id="category">
+                <h2 class="">Categories</h2>
+                <div class="category-grid-box row">
+                    {productsArr}
+                </div>
+            </section>
 
         </main>
     )
