d9884c2f889c64f7bd0b36a9217db3b65f7cf3b8
n01302646 <jobanjitsingh0706@gmail.com>
2020-07-22 02:03:11 -0400

    formatting

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -1,31 +1,28 @@
-  
+
 import React from 'react';
 import test from 'img/minni.jpg';
 
-const Product = ({name,price, imgpath}) => {
-	
-	
-	const ip = "img/"+ imgpath;
-	
-	//console.log("ip : "+ ip);
-	const sayHello = (event) => {
-		alert(`${name} has been added to your cart`)
-	}
-	
-	return (
-		<>
+const Product = ({ name, price, imgpath }) => {
 
-  <div>
-    <div>
-        <ul >
-            <li><a href="">{name}</a></li>
-            <li><a href="/productPage"><img src={test} alt={name} id='product-imagep' /></a></li>
-            <li><a href="/productPage">See More</a></li>
-        </ul>
-        <button  id='add-button' onClick={sayHello}>Add to cart</button>
-    </div>
-        </div>
-		</>
-	)
+
+    const sayHello = (event) => {
+        alert(`${name} has been added to your cart`)
+    }
+
+    return (
+        <>
+
+            <div>
+                <div>
+                    <ul >
+                        <li><a href="">{name}</a></li>
+                        <li><a href="/productPage"><img src={test} alt={name} id='product-imagep' /></a></li>
+                        <li><a href="/productPage">See More</a></li>
+                    </ul>
+                    <button id='add-button' onClick={sayHello}>Add to cart</button>
+                </div>
+            </div>
+        </>
+    )
 }
 export default Product;
