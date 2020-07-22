b0e816a0eb99b54f30bffd0970cd6661b4dd324a
n01302646 <jobanjitsingh0706@gmail.com>
2020-07-21 23:38:35 -0400

    package+header.js

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,31 +4,24 @@ import test from "img/test.png";
 const Header = () => {
 	return  (
 			<>
-			<header class="header">
-            <h1 class="hidden">Assignment 1 setup</h1>
-            
-            <nav class="navigation row" id="navigation">
-                
-                <a href="/"><img src="/images/amazon_logo.png" alt="Amazon Logo" class="navigation__website_logo" /></a>
+			 <header>
+        <div class="header-wrapper">
+            <div class="logo-wrapper">
+                <a href="/"><img src="./img/logo-amazon.png" alt="amazon-logo" class="img-logo"></a>
+                <div class="deals-box">
+                    <a href="#">Today's Deals: Electronic & Home Appliances</a>
+                </div>
+            </div>
 
-                <a href="/"><img src="/images/amazon_a_logo.png" alt="Amazon Logo" class="navigation__website_logo--mobile-view" /></a>
-
-                <ul class="navigation__ul-listing row" id="navigation__ul-listing">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#" class="text-capitalize">deal store</a></li>
-                    <li><a href="#" class="text-capitalize">best sellers</a></li>
-                    <li><a href="#" class="text-capitalize">products</a></li>
-                    <li><a href="#" class="text-capitalize">login</a></li>
-                    <li><a href="#" class="text-capitalize">sign up</a></li>
+            <nav class="main-nav">
+                <ul>
+                    <li class="deal-headers"><a href="">Mega Deals | Latest Deals</a></li>
+                    <li><button type="button"><a href="/login">Sign-in</a></button></li>
+                    <li><button><a href="/registration">Sign-up</a></button></li>
                 </ul>
-               
-                 <section class="section-hamburger">
-                    <h2 class="hidden">section-hamburger</h2>
-                    <img src="/images/hamburger.png" alt="hamburger menu" class="hamburger-menu" id="hamburger-menu" />
-                </section>
- 
             </nav>
-        </header>
+        </div>
+    </header>
             </>
 		)
 }
